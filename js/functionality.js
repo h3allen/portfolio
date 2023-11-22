bca167bdccf41d39a57477b5af322ea17705eddf
greembeems <hea4002@g.rit.edu>
2023-11-22 18:13:17 -0500

    Updated index to have slides & better mobile alignment

--- js/functionality.js
+++ js/functionality.js
@@ -23,4 +23,51 @@ window.onclick = function(event)
   }
 }
 
+window.onload = function() {
+  slideIndex = 1;
+  showSlides(slideIndex);
+};
+
+// Slides carousel
+let slideIndex = 0;
+
+// Next/previous slide
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image control
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) 
+  {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  {
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
+}
+/*function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+} */
 /* In case desired in future reference: https://jefferson-cuartas.medium.com/how-to-create-a-flip-card-effect-using-javascript-767dd945210c */
